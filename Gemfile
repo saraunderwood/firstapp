ca0a42196259c4b965128e0b5603e86468ea9b56
steph <steph@Studios-iMac.local>
2016-08-25 17:19:48 -0500

    my changes thus far...

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'stripe'
 
 gem 'simple_form'
 
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,32 @@
 class PostsController < ApplicationController
 
 	def new
-		@post = Post.new
+		if user_signed_in? 
+		    @post = Post.new
+		else
+			flash[:alert] = "You must be signed in to post!"
+			redirect_to :root
+		end
 	end
 
 	def create
-		@post = Post.create(post_params)
+		@post = current_user.posts.create(post_params)
 		if @post.save
 			redirect_to :root
 		else
 			flash[:alert] = "Failed to Create"
 			redirect_to :back
 		end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to :root
 	end
 
 	private
